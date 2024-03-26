3e8fcfd85452a4ead2e83f72b7a04da478f865b3
Jonathan Silva <jonathan.silva167@gmail.com>
2024-03-25 21:09:03 -0400

    merge develop complete

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import ResultsPage from './pages/ResultsPage/ResultsPage';
+import FormPage from './pages/FormPage/FormPage';
+import Header from './components/Header/Header';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/form' element={<FormPage />} />
+          <Route path='/results' element={<ResultsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
